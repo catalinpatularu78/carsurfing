85a06da2b575baf47c3e4645683842c99e6e6636
321gillian <gillian.middleton@gmail.com>
2023-03-28 19:33:09 +0100

    Initial booking elements

--- web-app/pages/rides/new.vue
+++ web-app/pages/rides/new.vue
@@ -106,32 +106,11 @@
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
           />
         </div>
-        <div
+        <ErrorMessage
           v-if="showError"
-          class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-6"
-          role="alert"
-        >
-          <strong class="font-bold">Error found</strong>
-          <span class="block"
-            >New journey could not be created. Please try again.</span
-          >
-          <span
-            @click="showError = false"
-            class="absolute top-0 bottom-0 right-0 px-4 py-3"
-          >
-            <svg
-              class="fill-current h-6 w-6 text-red-500"
-              role="button"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <title>Close</title>
-              <path
-                d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
-              />
-            </svg>
-          </span>
-        </div>
+          message="New journey could not be created. Please try again."
+          @hide-error="showError = false"
+        ></ErrorMessage>
         <button
           type="submit"
           class="text-white bg-purple-800 hover:bg-purple-900 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm w-full sm:w-full px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
@@ -140,34 +119,22 @@
         </button>
       </div>
     </form>
-    <div
+    <SuccessMessage
       v-else
-      class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
-      role="alert"
-    >
-      <div class="flex">
-        <div class="py-1">
-          <svg
-            class="fill-current h-6 w-6 text-teal-500 mr-4"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path
-              d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"
-            />
-          </svg>
-        </div>
-        <div>
-          <p class="font-bold">New journey created</p>
-          <p class="text-sm">We'll let you know when new carpoolers join!</p>
-        </div>
-      </div>
-    </div>
+      title="New journey created"
+      message="We'll let you know when new carpoolers join!"
+    ></SuccessMessage>
   </section>
 </template>
 <script>
 import { ref, computed } from "vue";
+import ErrorMessage from "~~/components/ErrorMessage.vue";
+import SuccessMessage from "~~/components/SuccessMessage.vue";
 export default {
+  components: {
+    ErrorMessage,
+    SuccessMessage,
+  },
   setup() {
     const rideCreated = ref(false);
     const showError = ref(false);
@@ -194,7 +161,7 @@ export default {
     }));
 
     async function submitCreateRide() {
-      await fetch("http://localhost:10555/rideapi/rides", {
+      await fetch("http://localhost:9091/rideapi/rides", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
