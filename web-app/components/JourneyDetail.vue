0e1a661dba0ea19a3dba96c709e24049f49fcdfd
321gillian <gillian.middleton@gmail.com>
2023-04-09 20:59:09 +0100

    Add rough UI for trips to profile

--- web-app/components/JourneyDetail.vue
+++ web-app/components/JourneyDetail.vue
@@ -1,18 +1,41 @@
 <template>
   <div>
-    <h3
-      class="text-sm font-semibold text-purple-500 lg:text-xl dark:text-white text-l pb-5"
-    >
-      {{ index + 1 }}. Trip on {{ ride.dateOfDeparture }}
-    </h3>
-    <JourneyDetailStepper
-      class="ml-8 mb-8"
-      :ride-details="ride"
-    ></JourneyDetailStepper>
+    <div class="flex items-center justify-between mb-6">
+      <div class="flex item-center pt-2">
+        <h3 class="text-6xl text-purple-500 uppercase">
+          {{ ride.fromLocation }}
+        </h3>
+        <Icon
+          name="material-symbols:arrow-forward-ios"
+          class="text-purple-800 h-12 w-12"
+        ></Icon>
+        <h3 class="text-6xl text-purple-500 uppercase">
+          {{ ride.toLocation }}
+        </h3>
+      </div>
+      <h3 class="text-2xl text-purple-800">
+        {{ formatDate(ride.dateOfDeparture) }}
+      </h3>
+    </div>
+    <div class="flex justify-between">
+      <JourneyDetailStepper
+        class="ml-8 mb-8"
+        :ride-details="ride"
+        :has-started="journeyIsInThePast"
+      ></JourneyDetailStepper>
+      <div class="flex flex-col justify-between">
+        <h4 class="text-lg text-right">Driver: {{ ride.driverId }}</h4>
+        <a
+          class="pb-6 text-purple-500 hover:underline cursor-pointer hover:text-purple-800"
+          >Cancel booking</a
+        >
+      </div>
+    </div>
     <hr class="w-auto h-0.5 bg-purple-500 opacity-50" />
   </div>
 </template>
 <script>
+import { computed } from "vue";
 import JourneyDetailStepper from "~~/components/JourneyDetailStepper.vue";
 export default {
   components: {
@@ -27,6 +50,25 @@ export default {
       type: Object,
       default: () => ({}),
     },
+  },
+  setup(props) {
+    const journeyIsInThePast = computed(() => {
+      const journeyDateTime = new Date(
+        `${props.ride.dateOfDeparture} ${props.ride.estimatedDepartureTime}`
+      );
+      const now = new Date();
+      return journeyDateTime < now;
+    });
+
+    function formatDate(date) {
+      const dateLeaving = new Date(date);
+      return dateLeaving.toDateString();
+    }
+
+    return {
+      journeyIsInThePast,
+      formatDate,
+    };
   },
 };
 </script>
--- web-app/components/JourneyDetailStepper.vue
+++ web-app/components/JourneyDetailStepper.vue
@@ -4,44 +4,39 @@
   >
     <li class="mb-10 ml-6 flex items-center">
       <span
-        class="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900"
+        class="absolute flex items-center justify-center w-8 h-8 bg-gray-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900"
       >
-        <svg
-          aria-hidden="true"
-          class="w-5 h-5 text-green-500 dark:text-green-400"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
+        <Icon name="material-symbols:alarm" class="text-xl"></Icon>
       </span>
-      <h3 class="font-medium leading-tight">{{ rideDetails.fromLocation }}</h3>
+      <h3 class="font-medium leading-tight">
+        Departure Time: {{ rideDetails.estimatedDepartureTime }}
+      </h3>
+    </li>
+    <li class="mb-10 ml-6 flex items-center" v-if="rideDetails.stop1">
+      <span
+        class="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-gray-700"
+      >
+        <Icon name="bi:sign-stop" class="text-2xl"></Icon>
+      </span>
+      <h3 class="font-medium leading-tight">
+        Stops: {{ rideDetails.stop1 }}
+        <span v-if="rideDetails.stop2">{{ ", " + rideDetails.stop2 }}</span>
+        <span v-if="rideDetails.stop3">{{ ", " + rideDetails.stop3 }}</span>
+      </h3>
     </li>
     <li class="ml-6 flex items-center">
       <span
         class="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-gray-700"
       >
-        <svg
-          aria-hidden="true"
-          class="w-5 h-5 text-gray-500 dark:text-gray-400"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
-          <path
-            fill-rule="evenodd"
-            d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm9.707 5.707a1 1 0 00-1.414-1.414L9 12.586l-1.293-1.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
+        <Icon name="fluent:people-audience-20-regular" class="text-2xl"></Icon>
       </span>
-      <h3 class="font-medium leading-tight">{{ rideDetails.toLocation }}</h3>
+      <h3 class="font-medium leading-tight">
+        {{
+          rideDetails.spacesLeft > 0
+            ? " Spaces Left: " + rideDetails.spacesLeft
+            : "Carpool is full"
+        }}
+      </h3>
     </li>
   </ol>
 </template>
