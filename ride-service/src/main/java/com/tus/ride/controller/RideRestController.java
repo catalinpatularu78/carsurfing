315f3ad363ba87418f6a1a770f179faea4f85ba4
Gustavo Pedro Ricou <63708303+Gustolandia@users.noreply.github.com>
2023-04-15 10:49:34 +0100

    ride service authentication update

--- ride-service/src/main/java/com/tus/ride/controller/RideRestController.java
+++ ride-service/src/main/java/com/tus/ride/controller/RideRestController.java
@@ -9,16 +9,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+<<<<<<< Updated upstream
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+=======
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+>>>>>>> Stashed changes
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +59,9 @@ public class RideRestController {
 
 	@Autowired
 	private BookingRepository bookingRepo;
+	
+	@Value("${userService.url}")
+	private String userServiceURL;
 
 	@Autowired
 	private RestTemplate restTemplate;
@@ -61,18 +75,49 @@ public class RideRestController {
 	}
 
 	@GetMapping("/rides/{id}")
-	public ResponseEntity<?> getRideById(@PathVariable(value = "id") Integer rideId) {
+	public ResponseEntity<?> getRideById(@PathVariable(value = "id") Integer rideId, @RequestHeader("Authorization") String header) {
+		try{
+			HttpHeaders headers = new HttpHeaders();
+		
+		 headers.set("Authorization",header);
+		 HttpEntity<String> entity = new HttpEntity<>("paramters",headers);
+
+		 RestTemplate restTemplate = new RestTemplate();
+		 ResponseEntity<Boolean> responseObj = restTemplate.exchange(userServiceURL + "generalauth", HttpMethod.GET, entity, Boolean.class);
+		 Boolean pass = responseObj.getBody();
+		 if (pass) {
+		
 		Optional<Ride> ride = rideRepo.findById(rideId);
 		if (ride.isPresent())
 			return ResponseEntity.ok().body(ride.get());
 		else
 			return ResponseEntity.ok().body("Ride not found");
+		 }
+		 else {
+			 return ResponseEntity
+			          .badRequest()
+			          .body(new String("Error: User not found!"));
+		 }
+		}catch(Exception e) {
+			return ResponseEntity
+			          .badRequest()
+			          .body(new String("Error!"));
+		}
 	}
 
 	@PostMapping(value = "/rides")
-	Response addRide(@RequestBody Ride ride) {
+	Response addRide(@RequestBody Ride ride, @RequestHeader("Authorization") String header) {
 		ResponseCode responseCode;
 		String responseMessage = "";
+		HttpHeaders headers = new HttpHeaders();
+		
+		 headers.set("Authorization",header);
+		 HttpEntity<String> entity = new HttpEntity<>("paramters",headers);
+
+		 RestTemplate restTemplate = new RestTemplate();
+		 ResponseEntity<Boolean> responseObj = restTemplate.exchange(userServiceURL + "generalauth", HttpMethod.GET, entity, Boolean.class);
+		 Boolean pass = responseObj.getBody();
+		 if (pass) {
 		try {
 			rideRepo.save(ride);
 			responseCode = ResponseCode.SUCCESS;
@@ -85,13 +130,31 @@ public class RideRestController {
 			responseMessage = "Ride from location " + ride.getFromLocation() + " to destination " + ride.getToLocation()
 					+ " cannot be added.";
 			return new ErrorResponse(responseCode, responseMessage, exc.getCause().getCause().getMessage());
-		}
+		}}
+		 else {
+			 responseCode = ResponseCode.FAILED;
+				responseMessage = "Ride from location " + ride.getFromLocation() + " to destination " + ride.getToLocation()
+						+ " cannot be added.";
+				return new ErrorResponse(responseCode, responseMessage, responseMessage);
+
+		 }
+		 
 	}
 
 	@DeleteMapping(value = "/rides/{id}")
-	Response deleteRide(@PathVariable("id") Integer id) {
+	Response deleteRide(@PathVariable("id") Integer id, @RequestHeader("Authorization") String header) {
 		ResponseCode responseCode;
 		String responseMessage = "";
+		HttpHeaders headers = new HttpHeaders();
+		
+		 headers.set("Authorization",header);
+		 HttpEntity<String> entity = new HttpEntity<>("paramters",headers);
+
+		 RestTemplate restTemplate = new RestTemplate();
+		 ResponseEntity<Boolean> responseObj = restTemplate.exchange(userServiceURL + "generalauth", HttpMethod.GET, entity, Boolean.class);
+		 Boolean pass = responseObj.getBody();
+		 if (pass) {
+		
 
 		try {
 			Optional<Ride> existingRide = rideRepo.findById(id);
@@ -105,12 +168,29 @@ public class RideRestController {
 			responseCode = ResponseCode.FAILED;
 			responseMessage = "Ride cannot be deleted.";
 			return new ErrorResponse(responseCode, responseMessage, exc.getCause().getCause().getMessage());
+		}}else {
+			responseCode = ResponseCode.FAILED;
+			responseMessage = "Ride cannot be deleted.";
+			return new ErrorResponse(responseCode, responseMessage, responseMessage);
 		}
 	}
 
 	@RequestMapping(value = "/ridesForRouteAndDate", method = RequestMethod.POST)
+<<<<<<< Updated upstream
 	public List<RideInfo> findRidePerRouteAndDate(@RequestBody Map<String, String> rideRequest) {
+=======
+	public ResponseEntity<?> findRidePerRouteAndDate(@RequestBody Map<String, String> rideRequest, @RequestHeader("Authorization") String header) {
+>>>>>>> Stashed changes
 		List<Ride> rides = new ArrayList<Ride>();
+		HttpHeaders headers = new HttpHeaders();
+		
+		 headers.set("Authorization",header);
+		 HttpEntity<String> entity = new HttpEntity<>("paramters",headers);
+
+		 RestTemplate restTemplate = new RestTemplate();
+		 ResponseEntity<Boolean> responseObj = restTemplate.exchange(userServiceURL + "generalauth", HttpMethod.GET, entity, Boolean.class);
+		 Boolean pass = responseObj.getBody();
+		 if (pass) {
 		if (!rideRequest.get("fromLocation").equalsIgnoreCase(rideRequest.get("toLocation"))) {
 			List<Ride> existingRides = rideRepo.findRides(rideRequest.get("fromLocation"),
 					rideRequest.get("toLocation"), rideRequest.get("dateOfDeparture"));
@@ -119,6 +199,7 @@ public class RideRestController {
 			}
 
 		}
+<<<<<<< Updated upstream
 		List<RideInfo> ridesInfo = new ArrayList<RideInfo>();
 		for (Ride ride : rides) {
 			User user = restTemplate.getForObject(userServiceURL + ride.getDriverId(), User.class);
@@ -130,24 +211,65 @@ public class RideRestController {
 	@GetMapping("rides/{fromLocation}/{toLocation}")
 	public List<RideInfo> findByLocation(@PathVariable("fromLocation") String fromLocation,
 			@PathVariable("toLocation") String toLocation) {
+=======
+		return ResponseEntity.ok().body(rides);
+		 }else {
+			 return ResponseEntity
+			          .badRequest()
+			          .body(new String("Error: User not found!"));
+		 }
+	}
+
+	@GetMapping("rides/{fromLocation}/{toLocation}")
+	public ResponseEntity<?> findByLocation(@PathVariable("fromLocation") String fromLocation,
+			@PathVariable("toLocation") String toLocation, @RequestHeader("Authorization") String header) {
+>>>>>>> Stashed changes
 		List<Ride> rides = new ArrayList<Ride>();
+		HttpHeaders headers = new HttpHeaders();
+		
+		 headers.set("Authorization",header);
+		 HttpEntity<String> entity = new HttpEntity<>("paramters",headers);
+
+		 RestTemplate restTemplate = new RestTemplate();
+		 ResponseEntity<Boolean> responseObj = restTemplate.exchange(userServiceURL + "generalauth", HttpMethod.GET, entity, Boolean.class);
+		 Boolean pass = responseObj.getBody();
+		 if (pass) {
 		if (!fromLocation.equalsIgnoreCase(toLocation)) {
 			List<Ride> existingRides = rideRepo.findByFromLocationAndToLocation(fromLocation, toLocation);
 			if (!existingRides.isEmpty()) {
 				rides = getRidesWithSpaces(existingRides);
 			}
 		}
+<<<<<<< Updated upstream
 		List<RideInfo> ridesInfo = new ArrayList<RideInfo>();
 		for (Ride ride : rides) {
 			User user = restTemplate.getForObject(userServiceURL + ride.getDriverId(), User.class);
 			ridesInfo.add(new RideInfo(ride, user));
 		}
 		return ridesInfo;
+=======
+		return ResponseEntity.ok().body(rides);
+		 }else {
+			 return ResponseEntity
+			          .badRequest()
+			          .body(new String("Error: User not found!"));
+		 }
+>>>>>>> Stashed changes
 	}
 
 	@Transactional(propagation = Propagation.MANDATORY)
 	@PostMapping(value = "/rides/booking")
-	Response addBooking(@RequestBody Booking booking) {
+	Response addBooking(@RequestBody Booking booking,  @RequestHeader("Authorization") String header) {
+		HttpHeaders headers = new HttpHeaders();
+		
+		 headers.set("Authorization",header);
+		 HttpEntity<String> entity = new HttpEntity<>("paramters",headers);
+
+		 RestTemplate restTemplate = new RestTemplate();
+		 ResponseEntity<Boolean> responseObj = restTemplate.exchange(userServiceURL + "generalauth", HttpMethod.GET, entity, Boolean.class);
+		 Boolean pass = responseObj.getBody();
+		 if (pass) {
+		
 		try {
 			// check if spaces are still available
 			Optional<Ride> existingRide = rideRepo.findById(booking.getRideId());
@@ -176,8 +298,9 @@ public class RideRestController {
 
 		} catch (Exception exc) {
 			return new ErrorResponse(ResponseCode.FAILED, "Ride cannot be booked.", exc.getMessage());
-		}
-
+		}}else {
+			 return new Response(ResponseCode.FAILED, "Ride cannot be booked.");
+		 }
 	}
 
 	// get only rides with least 1 space available
