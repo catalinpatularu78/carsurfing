dd341aa9728ff6b45648e752d7e9efe4e27a0cf8
Gustavo Pedro Ricou <63708303+Gustolandia@users.noreply.github.com>
2023-04-25 23:58:28 +0100

    Eureka service (docker file and docker compose alteration missing) (run locally)

--- ride-service/src/main/java/com/tus/ride/RideServiceApplication.java
+++ ride-service/src/main/java/com/tus/ride/RideServiceApplication.java
@@ -1,9 +1,17 @@
 package com.tus.ride;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -19,4 +27,17 @@ public class RideServiceApplication {
 		SpringApplication.run(RideServiceApplication.class, args);
 	}
 
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
 }
